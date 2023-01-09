2ff1d9cdffe0581dfb67e426c0500bc634641193
brown2206 <brown.angelo.jr@gmail.com>
2023-01-08 21:07:04 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ]
-    
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return (
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            <div className='comments'>
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackList feedback={feedback}/>
             </div>
-        </div>
+        </>
     )
 }
 
